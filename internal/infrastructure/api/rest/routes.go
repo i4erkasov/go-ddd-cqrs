06f38b2b588419f93ecbd8e423092dd9f99ae405
i4erkasov <i.4erkasov@gmail.com>
2024-02-20 22:53:25 +0200

    review description

--- internal/infrastructure/api/rest/routes.go
+++ internal/infrastructure/api/rest/routes.go
@@ -1,18 +1,1 @@
 package rest
-
-import (
-	"net/http"
-
-	"github.com/i4erkasov/go-ddd-cqrs/internal/infrastructure/api/http/handler"
-	"github.com/i4erkasov/go-ddd-cqrs/internal/infrastructure/api/http/middleware"
-	"github.com/labstack/echo/v4"
-)
-
-func (s *Server) routes(h *handler.HttpHandler, m *middleware.HttpMiddleware) {
-	s.echo.GET("/health", func(c echo.Context) error {
-		return c.NoContent(http.StatusOK)
-	})
-
-	api := s.echo.Group("/api")
-	api.GET("/hello", h.HelloWold)
-}
