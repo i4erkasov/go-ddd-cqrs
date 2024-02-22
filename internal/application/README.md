8d512a4a214f65d6ec65f355ac021e52922fce36
i4erkasov <i.4erkasov@gmail.com>
2024-02-22 22:23:50 +0200

    part-1

--- internal/application/README.md
+++
@@ -1,9 +0,0 @@
-# Application
-
-Here you'll find the application configuration and initialization, command and query handling in accordance with CQRS.
-
-## Components
-
-- **app.go**: Application configuration and initialization.
-- **command/command.go**: Command handling (CQRS).
-- **query/query.go**: Query handling (CQRS).
--- internal/genmocks.go
+++
@@ -1,8 +0,0 @@
-package internal
-
-// Deprecated: in mockery v3 this approach will be removed
-// rename the file backend/.mockery.yaml.dist to backend/.mockery.yaml and remove go:generate
-// there's a problem with aliased type: for aliased types mocks won't be generated
-// type Foo = baz.Baz - this mock won't be generated
-// see https://github.com/vektra/mockery/issues with replace-type params
-//go:generate mockery --name=(.+)Mock --case=underscore
